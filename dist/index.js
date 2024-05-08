b5a128eec0fbf67cdec77bc92a01c4c3a621c99c
milos-ethernal <milos.maksimovic@ethernal.tech>
2024-05-08 12:35:39 +0200

    Build

--- dist/index.js
+++ dist/index.js
@@ -1,372 +1,31 @@
 import { createRequire as __WEBPACK_EXTERNAL_createRequire } from "module";
 /******/ var __webpack_modules__ = ({
 
-/***/ 437:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-const fs = __nccwpck_require__(147)
-const path = __nccwpck_require__(17)
-const os = __nccwpck_require__(37)
-const crypto = __nccwpck_require__(113)
-const packageJson = __nccwpck_require__(968)
-
-const version = packageJson.version
-
-const LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg
-
-// Parse src into an Object
-function parse (src) {
-  const obj = {}
-
-  // Convert buffer to string
-  let lines = src.toString()
-
-  // Convert line breaks to same format
-  lines = lines.replace(/\r\n?/mg, '\n')
-
-  let match
-  while ((match = LINE.exec(lines)) != null) {
-    const key = match[1]
-
-    // Default undefined or null to empty string
-    let value = (match[2] || '')
-
-    // Remove whitespace
-    value = value.trim()
-
-    // Check if double quoted
-    const maybeQuote = value[0]
-
-    // Remove surrounding quotes
-    value = value.replace(/^(['"`])([\s\S]*)\1$/mg, '$2')
-
-    // Expand newlines if double quoted
-    if (maybeQuote === '"') {
-      value = value.replace(/\\n/g, '\n')
-      value = value.replace(/\\r/g, '\r')
-    }
-
-    // Add to object
-    obj[key] = value
-  }
-
-  return obj
-}
-
-function _parseVault (options) {
-  const vaultPath = _vaultPath(options)
-
-  // Parse .env.vault
-  const result = DotenvModule.configDotenv({ path: vaultPath })
-  if (!result.parsed) {
-    throw new Error(`MISSING_DATA: Cannot parse ${vaultPath} for an unknown reason`)
-  }
-
-  // handle scenario for comma separated keys - for use with key rotation
-  // example: DOTENV_KEY="dotenv://:key_1234@dotenv.org/vault/.env.vault?environment=prod,dotenv://:key_7890@dotenv.org/vault/.env.vault?environment=prod"
-  const keys = _dotenvKey(options).split(',')
-  const length = keys.length
-
-  let decrypted
-  for (let i = 0; i < length; i++) {
-    try {
-      // Get full key
-      const key = keys[i].trim()
-
-      // Get instructions for decrypt
-      const attrs = _instructions(result, key)
-
-      // Decrypt
-      decrypted = DotenvModule.decrypt(attrs.ciphertext, attrs.key)
-
-      break
-    } catch (error) {
-      // last key
-      if (i + 1 >= length) {
-        throw error
-      }
-      // try next key
-    }
-  }
-
-  // Parse decrypted .env string
-  return DotenvModule.parse(decrypted)
-}
-
-function _log (message) {
-  console.log(`[dotenv@${version}][INFO] ${message}`)
-}
-
-function _warn (message) {
-  console.log(`[dotenv@${version}][WARN] ${message}`)
-}
-
-function _debug (message) {
-  console.log(`[dotenv@${version}][DEBUG] ${message}`)
-}
-
-function _dotenvKey (options) {
-  // prioritize developer directly setting options.DOTENV_KEY
-  if (options && options.DOTENV_KEY && options.DOTENV_KEY.length > 0) {
-    return options.DOTENV_KEY
-  }
-
-  // secondary infra already contains a DOTENV_KEY environment variable
-  if (process.env.DOTENV_KEY && process.env.DOTENV_KEY.length > 0) {
-    return process.env.DOTENV_KEY
-  }
-
-  // fallback to empty string
-  return ''
-}
-
-function _instructions (result, dotenvKey) {
-  // Parse DOTENV_KEY. Format is a URI
-  let uri
-  try {
-    uri = new URL(dotenvKey)
-  } catch (error) {
-    if (error.code === 'ERR_INVALID_URL') {
-      throw new Error('INVALID_DOTENV_KEY: Wrong format. Must be in valid uri format like dotenv://:key_1234@dotenv.org/vault/.env.vault?environment=development')
-    }
-
-    throw error
-  }
-
-  // Get decrypt key
-  const key = uri.password
-  if (!key) {
-    throw new Error('INVALID_DOTENV_KEY: Missing key part')
-  }
-
-  // Get environment
-  const environment = uri.searchParams.get('environment')
-  if (!environment) {
-    throw new Error('INVALID_DOTENV_KEY: Missing environment part')
-  }
-
-  // Get ciphertext payload
-  const environmentKey = `DOTENV_VAULT_${environment.toUpperCase()}`
-  const ciphertext = result.parsed[environmentKey] // DOTENV_VAULT_PRODUCTION
-  if (!ciphertext) {
-    throw new Error(`NOT_FOUND_DOTENV_ENVIRONMENT: Cannot locate environment ${environmentKey} in your .env.vault file.`)
-  }
-
-  return { ciphertext, key }
-}
-
-function _vaultPath (options) {
-  let dotenvPath = path.resolve(process.cwd(), '.env')
-
-  if (options && options.path && options.path.length > 0) {
-    dotenvPath = options.path
-  }
-
-  // Locate .env.vault
-  return dotenvPath.endsWith('.vault') ? dotenvPath : `${dotenvPath}.vault`
-}
-
-function _resolveHome (envPath) {
-  return envPath[0] === '~' ? path.join(os.homedir(), envPath.slice(1)) : envPath
-}
-
-function _configVault (options) {
-  _log('Loading env from encrypted .env.vault')
-
-  const parsed = DotenvModule._parseVault(options)
-
-  let processEnv = process.env
-  if (options && options.processEnv != null) {
-    processEnv = options.processEnv
-  }
-
-  DotenvModule.populate(processEnv, parsed, options)
-
-  return { parsed }
-}
-
-function configDotenv (options) {
-  let dotenvPath = path.resolve(process.cwd(), '.env')
-  let encoding = 'utf8'
-  const debug = Boolean(options && options.debug)
-
-  if (options) {
-    if (options.path != null) {
-      dotenvPath = _resolveHome(options.path)
-    }
-    if (options.encoding != null) {
-      encoding = options.encoding
-    }
-  }
-
-  try {
-    // Specifying an encoding returns a string instead of a buffer
-    const parsed = DotenvModule.parse(fs.readFileSync(dotenvPath, { encoding }))
-
-    let processEnv = process.env
-    if (options && options.processEnv != null) {
-      processEnv = options.processEnv
-    }
-
-    DotenvModule.populate(processEnv, parsed, options)
-
-    return { parsed }
-  } catch (e) {
-    if (debug) {
-      _debug(`Failed to load ${dotenvPath} ${e.message}`)
-    }
-
-    return { error: e }
-  }
-}
-
-// Populates process.env from .env file
-function config (options) {
-  const vaultPath = _vaultPath(options)
-
-  // fallback to original dotenv if DOTENV_KEY is not set
-  if (_dotenvKey(options).length === 0) {
-    return DotenvModule.configDotenv(options)
-  }
-
-  // dotenvKey exists but .env.vault file does not exist
-  if (!fs.existsSync(vaultPath)) {
-    _warn(`You set DOTENV_KEY but you are missing a .env.vault file at ${vaultPath}. Did you forget to build it?`)
-
-    return DotenvModule.configDotenv(options)
-  }
-
-  return DotenvModule._configVault(options)
-}
-
-function decrypt (encrypted, keyStr) {
-  const key = Buffer.from(keyStr.slice(-64), 'hex')
-  let ciphertext = Buffer.from(encrypted, 'base64')
-
-  const nonce = ciphertext.slice(0, 12)
-  const authTag = ciphertext.slice(-16)
-  ciphertext = ciphertext.slice(12, -16)
-
-  try {
-    const aesgcm = crypto.createDecipheriv('aes-256-gcm', key, nonce)
-    aesgcm.setAuthTag(authTag)
-    return `${aesgcm.update(ciphertext)}${aesgcm.final()}`
-  } catch (error) {
-    const isRange = error instanceof RangeError
-    const invalidKeyLength = error.message === 'Invalid key length'
-    const decryptionFailed = error.message === 'Unsupported state or unable to authenticate data'
-
-    if (isRange || invalidKeyLength) {
-      const msg = 'INVALID_DOTENV_KEY: It must be 64 characters long (or more)'
-      throw new Error(msg)
-    } else if (decryptionFailed) {
-      const msg = 'DECRYPTION_FAILED: Please check your DOTENV_KEY'
-      throw new Error(msg)
-    } else {
-      console.error('Error: ', error.code)
-      console.error('Error: ', error.message)
-      throw error
-    }
-  }
-}
-
-// Populate process.env with parsed values
-function populate (processEnv, parsed, options = {}) {
-  const debug = Boolean(options && options.debug)
-  const override = Boolean(options && options.override)
-
-  if (typeof parsed !== 'object') {
-    throw new Error('OBJECT_REQUIRED: Please check the processEnv argument being passed to populate')
-  }
-
-  // Set process.env
-  for (const key of Object.keys(parsed)) {
-    if (Object.prototype.hasOwnProperty.call(processEnv, key)) {
-      if (override === true) {
-        processEnv[key] = parsed[key]
-      }
-
-      if (debug) {
-        if (override === true) {
-          _debug(`"${key}" is already defined and WAS overwritten`)
-        } else {
-          _debug(`"${key}" is already defined and was NOT overwritten`)
-        }
-      }
-    } else {
-      processEnv[key] = parsed[key]
-    }
-  }
-}
-
-const DotenvModule = {
-  configDotenv,
-  _configVault,
-  _parseVault,
-  config,
-  decrypt,
-  parse,
-  populate
-}
-
-module.exports.configDotenv = DotenvModule.configDotenv
-module.exports._configVault = DotenvModule._configVault
-module.exports._parseVault = DotenvModule._parseVault
-module.exports.config = DotenvModule.config
-module.exports.decrypt = DotenvModule.decrypt
-module.exports.parse = DotenvModule.parse
-module.exports.populate = DotenvModule.populate
-
-module.exports = DotenvModule
-
-
-/***/ }),
-
-/***/ 113:
+/***/ 341:
 /***/ ((module) => {
 
-module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("crypto");
+module.exports = eval("require")("dotenv");
+
 
 /***/ }),
 
-/***/ 147:
-/***/ ((module) => {
-
-module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("fs");
-
-/***/ }),
-
-/***/ 37:
-/***/ ((module) => {
-
-module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("os");
-
-/***/ }),
-
-/***/ 17:
-/***/ ((module) => {
-
-module.exports = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("path");
-
-/***/ }),
-
-/***/ 949:
+/***/ 795:
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __nccwpck_require__) => {
 
 
 // EXPORTS
 __nccwpck_require__.d(__webpack_exports__, {
-  "Ph": () => (/* binding */ downloadLatestRelease),
+  "g3": () => (/* binding */ downloadRelease),
   "oH": () => (/* binding */ moveToRunnerBin),
-  "Id": () => (/* binding */ unpackLatestRelease)
+  "tG": () => (/* binding */ unpackRelease)
 });
 
-// EXTERNAL MODULE: external "fs"
-var external_fs_ = __nccwpck_require__(147);
+;// CONCATENATED MODULE: external "fs"
+const external_fs_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("fs");
 ;// CONCATENATED MODULE: external "url"
 const external_url_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("url");
-// EXTERNAL MODULE: external "path"
-var external_path_ = __nccwpck_require__(17);
+;// CONCATENATED MODULE: external "path"
+const external_path_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("path");
 ;// CONCATENATED MODULE: external "child_process"
 const external_child_process_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.meta.url)("child_process");
 ;// CONCATENATED MODULE: external "util"
@@ -380,35 +39,28 @@ const external_util_namespaceObject = __WEBPACK_EXTERNAL_createRequire(import.me
 
 const exec = (0,external_util_namespaceObject.promisify)(external_child_process_namespaceObject.exec);
 
-const BINS_BASE_URL = 'https://github.com/cardano-foundation/cardano-wallet';
-
-const get_latest_release_tag = async () => {
-    const response = await fetch(`${BINS_BASE_URL}/releases/latest`, { method: 'GET',
-        headers: { 'Accept': 'application/json' }
-    });
-    const data = await response.json();
-    return data.tag_name;
-};
+const CARDANO_NODE_VERSION = "8.7.3"
+const BINS_BASE_URL = 'https://github.com/IntersectMBO/cardano-node/releases/tag/';
 
 const getPlatformReleaseUrl = async () => {
     const platform = process.platform;
-    const tag = await get_latest_release_tag();
+    const tag = CARDANO_NODE_VERSION;
     let file_name = '';
     if (platform === 'linux') {
-        file_name = `cardano-wallet-${tag}-linux64.tar.gz`;
+        file_name = `cardano-node-${tag}-linux64.tar.gz`;
     }
     else if (platform === 'darwin') {
-        file_name = `cardano-wallet-${tag}-macos-intel.tar.gz`;
+        file_name = `cardano-node-${tag}-macos.tar.gz`;
     }
     else if (platform === 'win32') {
-        file_name = `cardano-wallet-${tag}-win64.zip`;
+        file_name = `cardano-node-${tag}-win64.zip`;
     }
     else {
         throw new Error(`Platform ${platform} not supported`);
     }
     return `${BINS_BASE_URL}/releases/download/${tag}/${file_name}`;
 };
-const downloadLatestRelease = async () => {
+const downloadRelease = async () => {
     const url = await getPlatformReleaseUrl();
     const response = await fetch(url);
     const buffer = await response.arrayBuffer();
@@ -418,11 +70,11 @@ const downloadLatestRelease = async () => {
         throw new Error('Unable to determine the file name from the URL');
     }
     const dir = './bins';
-    (0,external_fs_.mkdirSync)(dir, { recursive: true });
-    const filePath = external_path_.join(dir, file_name);
-    (0,external_fs_.writeFileSync)(filePath, Buffer.from(buffer));
+    (0,external_fs_namespaceObject.mkdirSync)(dir, { recursive: true });
+    const filePath = external_path_namespaceObject.join(dir, file_name);
+    (0,external_fs_namespaceObject.writeFileSync)(filePath, Buffer.from(buffer));
 };
-const unpackLatestRelease = async () => {
+const unpackRelease = async () => {
     const url = await getPlatformReleaseUrl();
     const urlObj = new external_url_namespaceObject.URL(url);
     const file_name = urlObj.pathname.split('/').pop();
@@ -430,18 +82,18 @@ const unpackLatestRelease = async () => {
         throw new Error('Unable to determine the file name from the URL');
     }
     const dir = './bins';
-    const filePath = external_path_.join(dir, file_name);
+    const filePath = external_path_namespaceObject.join(dir, file_name);
     try {
         if (['linux', 'darwin', 'win32'].includes(process.platform)) {
             await exec(`tar -xf "${filePath}" -C "${dir}"`);
-            
+
             // Assuming the tar archive contains a single top-level directory
-            const files = (0,external_fs_.readdirSync)(dir);
-            const extractedDir = files.find(file => (0,external_fs_.statSync)(external_path_.join(dir, file)).isDirectory());
+            const files = (0,external_fs_namespaceObject.readdirSync)(dir);
+            const extractedDir = files.find(file => (0,external_fs_namespaceObject.statSync)(external_path_namespaceObject.join(dir, file)).isDirectory());
 
             if (extractedDir) {
-                await exec(`mv "${external_path_.join(dir, extractedDir)}"/* "${dir}"`);
-                (0,external_fs_.rmdirSync)(external_path_.join(dir, extractedDir));
+                await exec(`mv "${external_path_namespaceObject.join(dir, extractedDir)}"/* "${dir}"`);
+                (0,external_fs_namespaceObject.rmdirSync)(external_path_namespaceObject.join(dir, extractedDir));
             }
         } else {
             throw new Error(`Platform ${process.platform} not supported`);
@@ -466,29 +118,22 @@ const moveToRunnerBin = async () => {
 
 /***/ }),
 
-/***/ 633:
+/***/ 490:
 /***/ ((__webpack_module__, __unused_webpack___webpack_exports__, __nccwpck_require__) => {
 
 __nccwpck_require__.a(__webpack_module__, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
-/* harmony import */ var dotenv__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(437);
-/* harmony import */ var _functions_cardano_bins_js__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(949);
+/* harmony import */ var dotenv__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(341);
+/* harmony import */ var _functions_cardano_bins_js__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(795);
 
 
 
 dotenv__WEBPACK_IMPORTED_MODULE_0__.config();
-await (0,_functions_cardano_bins_js__WEBPACK_IMPORTED_MODULE_1__/* .downloadLatestRelease */ .Ph)();
-await (0,_functions_cardano_bins_js__WEBPACK_IMPORTED_MODULE_1__/* .unpackLatestRelease */ .Id)();
+await (0,_functions_cardano_bins_js__WEBPACK_IMPORTED_MODULE_1__/* .downloadRelease */ .g3)();
+await (0,_functions_cardano_bins_js__WEBPACK_IMPORTED_MODULE_1__/* .unpackRelease */ .tG)();
 await (0,_functions_cardano_bins_js__WEBPACK_IMPORTED_MODULE_1__/* .moveToRunnerBin */ .oH)();
 //await appendToGitHubPath(fullPath);
 __webpack_async_result__();
 } catch(e) { __webpack_async_result__(e); } }, 1);
-
-/***/ }),
-
-/***/ 968:
-/***/ ((module) => {
-
-module.exports = JSON.parse('{"name":"dotenv","version":"16.3.1","description":"Loads environment variables from .env file","main":"lib/main.js","types":"lib/main.d.ts","exports":{".":{"types":"./lib/main.d.ts","require":"./lib/main.js","default":"./lib/main.js"},"./config":"./config.js","./config.js":"./config.js","./lib/env-options":"./lib/env-options.js","./lib/env-options.js":"./lib/env-options.js","./lib/cli-options":"./lib/cli-options.js","./lib/cli-options.js":"./lib/cli-options.js","./package.json":"./package.json"},"scripts":{"dts-check":"tsc --project tests/types/tsconfig.json","lint":"standard","lint-readme":"standard-markdown","pretest":"npm run lint && npm run dts-check","test":"tap tests/*.js --100 -Rspec","prerelease":"npm test","release":"standard-version"},"repository":{"type":"git","url":"git://github.com/motdotla/dotenv.git"},"funding":"https://github.com/motdotla/dotenv?sponsor=1","keywords":["dotenv","env",".env","environment","variables","config","settings"],"readmeFilename":"README.md","license":"BSD-2-Clause","devDependencies":{"@definitelytyped/dtslint":"^0.0.133","@types/node":"^18.11.3","decache":"^4.6.1","sinon":"^14.0.1","standard":"^17.0.0","standard-markdown":"^7.1.0","standard-version":"^9.5.0","tap":"^16.3.0","tar":"^6.1.11","typescript":"^4.8.4"},"engines":{"node":">=12"},"browser":{"fs":false}}');
 
 /***/ })
 
@@ -620,6 +265,6 @@ module.exports = JSON.parse('{"name":"dotenv","version":"16.3.1","description":"
 /******/ // startup
 /******/ // Load entry module and return exports
 /******/ // This entry module used 'module' so it can't be inlined
-/******/ var __webpack_exports__ = __nccwpck_require__(633);
+/******/ var __webpack_exports__ = __nccwpck_require__(490);
 /******/ __webpack_exports__ = await __webpack_exports__;
 /******/ 
